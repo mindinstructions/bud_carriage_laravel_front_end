a47698f700aa81bf629915674f174608f1bb2ec4
Noumankhalid9 <numan7778@gmail.com>
2021-09-10 11:16:10 +0500

    signup and login page template in html and css

--- login.php
+++ login.php
@@ -1,7 +1,25 @@
 <?php include("includes/header.php"); ?>
 
-	<div class="container-bg-home">
-		<h1>Login Page Starts Here</h1>
+	<div class="container-bg-home bud-logsp">
+		<a href="#"><img src="img/logo.png"></a>
+		<h2 class="center bud-heading">Welcome back</h2>
+        <div class="login-form">
+        	<input type="text" name="email" placeholder="Email or username">
+        	<input type="number" name="password" placeholder="Password">
+        	<input type="button" name="login" value="Log in">
+        	<div class="account-access">
+        	<a href="#">Having trouble accessing your account? Click here</a>
+            </div>
+        	-or-
+        	<div class="social-login">
+        	<a href="#" class="fa fa-facebook"><span>Continue with Facebook</span></a>
+            <a href="#" class="fa fa-google"><span>Continue with  Google</span></a>
+            <a href="#" class="fa fa-apple"><span>Continue with  Apple</span></a>
+            </div>
+            <div class="account-access">
+        	<a href="signup.php">New to Weedmaps? Sign up</a>
+            </div>
+        </div>
 	</div>
 
 <?php include("includes/footer.php"); ?>
--- signup.php
+++ signup.php
@@ -1,7 +1,33 @@
 <?php include("includes/header.php"); ?>
 
-	<div class="container-bg-home">
-		<h1>I am Sign up page</h1>
+	<div class="container-bg-home bud-logsp">
+		<a href="#"><img src="img/logo.png"></a>
+		<h2 class="center bud-heading">Sign Up</h2>
+		 <div class="login-form">
+        	<input type="text" name="email" placeholder="Email">
+        	<input type="number" name="password" placeholder="Password">
+        	<input type="text" name="u-name" placeholder="User Name">
+        	<input type="button" name="signup" value="Sign Up">
+        	<div class="account-access">
+        	<a href="login.php">Already a member? Log in</a>
+            </div>
+        	-or-
+        	<div class="social-login">
+        	<a href="#" class="fa fa-facebook"><span>Continue with Facebook</span></a>
+            <a href="#" class="fa fa-google"><span>Continue with  Google</span></a>
+            <a href="#" class="fa fa-apple"><span>Continue with  Apple</span></a>
+            </div>
+            <div class="account-access">
+        	<p>Weedmaps respects privacy. Names and emails aren't displayed publicly, and nothing is posted to your Facebook or Google account without permission.</p>
+            </div>
+            <h3 class="w-singup">Why sign up?</h3>
+            <img src="img/signup-baner.jpg">
+            <ul class="signup-list">
+            	<li>Find the best dispensary storefronts and see who delivers to you.</li>
+            	<li>Get updates about your favorite products, brands, and retailers.</li>
+            	<li>Leave reviews & share your experiences to help out the community.</li>
+            </ul>
+        </div>
 	</div>
 
 <?php include("includes/footer.php"); ?>
